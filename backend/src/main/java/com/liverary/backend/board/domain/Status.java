946e67f35154560f31f2cdc079fda3c5ed0226c2
이수인 <toto0887@gmail.com>
2026-01-20 13:51:51 +0900

    refactor(board): introduce POSTED status

--- backend/src/main/java/com/liverary/backend/board/domain/Status.java
+++ backend/src/main/java/com/liverary/backend/board/domain/Status.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * 게시글 상태 (답변대기, 답변완료)
+ * 게시글 상태 (답변대기, 답변완료, 게시완료)
  */
 @Getter
 @AllArgsConstructor
 public enum Status {
+    // 문의글
     PENDING("답변대기"),
-    DONE("답변완료");
+    DONE("답변완료"),
+    // 공지, 홍보글
+    POSTED("게시완료");
 
     private final String description;
 }
