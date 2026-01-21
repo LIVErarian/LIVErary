fe7fd219a71fc6ce85e362e0a03692130cbc605a
K-DaHee <dhk086512@gmail.com>
2026-01-21 17:10:55 +0900

    refactor(user): change API endpoint path from /api/user to /user (S14P11A307-67)

--- backend/src/main/java/com/liverary/backend/user/controller/UserController.java
+++ backend/src/main/java/com/liverary/backend/user/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
