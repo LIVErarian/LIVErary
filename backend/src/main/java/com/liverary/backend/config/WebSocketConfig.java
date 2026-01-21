0538c1baabadb74e170f539cb56faa354c16d45c
DESKTOP-6OO0P6F\SSAFY <gaau511@gmail.com>
2026-01-21 12:34:51 +0900

    feat(config): Add WebSocket configuration (S14P11A307-37)

---
+++ backend/src/main/java/com/liverary/backend/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.liverary.backend.config;
+
+import com.liverary.backend.auth.provider.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(new StompHandshakeHandler(jwtProvider));
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
