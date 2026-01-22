e3bb71d0022a521157d56f04d0e11fcd48f6e100
DESKTOP-6OO0P6F\SSAFY <gaau511@gmail.com>
2026-01-22 09:59:25 +0900

    feat(rtc): Add ConnectRoomRequest to let users can inform room entrance (S14P11A307-81)

---
+++ backend/src/main/java/com/liverary/backend/socket/dto/request/ConnectRoomRequest.java
@@ -0,0 +1,17 @@
+package com.liverary.backend.socket.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 방 참여 요청 DTO.
+ */
+@Getter
+@NoArgsConstructor
+public class ConnectRoomRequest {
+
+    // 사용자가 참여할 방 ID
+    private UUID roomId;
+}
