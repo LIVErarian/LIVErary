a4360a3ecad469abb817245eb15312ab8a325549
Sy3058 <apo1803058@gmail.com>
2026-01-27 17:01:01 +0900

    feat(network): implement axios response interceptor for token refresh

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,10 @@
-import axios, { type InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError, type InternalAxiosRequestConfig } from 'axios';
 
 import { useAuthStore } from '@/store/useAuthStore';
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
@@ -24,6 +28,43 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Response Interceptor
+api.interceptors.response.use(
+  // 200인 경우 그냥 통과
+  (response) => response,
+
+  // 에러 처리
+  async (error: AxiosError) => {
+    const originalRequest = error.config as CustomAxiosRequestConfig;
+
+    // originalRequest가 없거나 이미 재시도했으면 에러 던지기
+    if (!originalRequest || originalRequest._retry) {
+      return Promise.reject(error);
+    }
+
+    if (error.response?.status === 401) {
+      originalRequest._retry = true;
+
+      try {
+        // refresh token 요청
+        const { data } = await api.post('/auth/reissue');
+        const refreshToken = data.refreshToken;
+        useAuthStore.getState().setAccessToken(refreshToken);
+
+        // 토큰 교체해서 다시 요청
+        originalRequest.headers.Authorization = `Bearer ${refreshToken}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        useAuthStore.getState().logout();
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   },
 );
