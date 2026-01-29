08f51a582802532fdca49d0805fae534458593a3
Sy3058 <apo1803058@gmail.com>
2026-01-29 09:50:31 +0900

    refactor(user): remove gender from user type

--- frontend/src/types/user.types.ts
+++ frontend/src/types/user.types.ts
@@ -1,11 +1,9 @@
 export type UserRole = 'USER' | 'ADMIN';
-export type Gender = 'MALE' | 'FEMALE';
 
 export interface UserProfile {
   userId: string; // back에서 지정하는 uuid
   email: string;
   nickname: string;
-  gender: Gender;
   role: UserRole;
   totalReadingTime: number; // 분 단위
   bookCounts: {
