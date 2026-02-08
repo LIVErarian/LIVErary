84c512a5950fee074c1f9a3312517adca75449d6
안수연 <apo1803058@gmail.com>
2026-02-09 03:39:47 +0900

    feat(metaverse): implement concentrate modal

--- frontend/src/pages/game/GamePage.tsx
+++ frontend/src/pages/game/GamePage.tsx
@@ -12,7 +12,7 @@ export const GamePage = () => {
     const initAssets = async () => {
       try {
         const minTimePromise = new Promise(
-          (resolve) => setTimeout(resolve, 5000), // 5초 대기
+          (resolve) => setTimeout(resolve, 2000), // 2초 대기
         );
 
         const assetLoadPromise = preloadAssets((progress) => {
