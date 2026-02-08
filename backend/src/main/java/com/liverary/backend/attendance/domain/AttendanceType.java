8db55542935d9529b8af85bf03981adf7f4df43b
star0865 <dhk086512@gmail.com>
2026-02-08 14:58:45 +0900

    refactor(attendance): add type description (S14P11A307-275)

--- backend/src/main/java/com/liverary/backend/attendance/domain/AttendanceType.java
+++ backend/src/main/java/com/liverary/backend/attendance/domain/AttendanceType.java
@@ -1,9 +1,16 @@
 package com.liverary.backend.attendance.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * 출석 타입을 정의하는 Enum
  */
+@Getter
+@AllArgsConstructor
 public enum AttendanceType {
-    DAILY, // 일일 출석
-    READING // 독서 출석 (30분 이상 독서 시 자동 기록)
+    DAILY("일일 출석"),
+    READING("독서 출석");
+
+    private final String description;
 }
